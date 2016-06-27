717194a446190b26c8199b102a66e51df10c75c7
Nelson Leung <nleung91@gmail.com>
2016-06-27 13:56:01 -0500

    update build to include matexp for vecotrs and matexp for scaling and squaring

--- custom_kernels/build.sh
+++ custom_kernels/build.sh
@@ -13,8 +13,23 @@ nvcc -std=c++11 -c -o matmul.cu.o ../matmul.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x
 
 nvcc -std=c++11 -c -o matadd_coeff_v2.cu.o ../matadd_coeff_v2.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
+nvcc -std=c++11 -c -o matadd_coeff_v3.cu.o ../matadd_coeff_v3.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+nvcc -std=c++11 -c -o matadd_coeff_v4.cu.o ../matadd_coeff_v4.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
 g++ -std=c++11 -shared -o matrix_exp.so ../matrix_exp.cc -fPIC -I $TF_INC
 
 g++ -std=c++11 -shared -o cuda_matexp.so ../cuda_matexp.cc matadd_coeff.cu.o matmul.cu.o matadd_prepare.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -L /usr/local/cuda/lib64 -lcudart
 
 g++ -std=c++11 -shared -o cuda_matexp_v2.so ../cuda_matexp_v2.cc matadd_coeff.cu.o matmul.cu.o matadd_coeff_v2.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -L /usr/local/cuda/lib64 -lcudart
+
+g++ -std=c++11 -shared -o cuda_matexp_v3.so ../cuda_matexp_v3.cc matadd_coeff.cu.o matmul.cu.o matadd_coeff_v2.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -lcudart
+
+g++ -std=c++11 -shared -o cuda_matexp_v4.so ../cuda_matexp_v4.cc matadd_coeff.cu.o matmul.cu.o matadd_coeff_v2.cu.o matadd_coeff_v4.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -lcudart
+
+nvcc -std=c++11 -c -o matmul_v2.cu.o ../matmul_v2.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+g++ -std=c++11 -shared -o cuda_matexp_vecs.so ../cuda_matexp_vecs.cc matadd_coeff_v3.cu.o matmul_v2.cu.o matadd_coeff_v2.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -lcudart
+
+g++ -std=c++11 -shared -o cuda_matexp_vecs_grads.so ../cuda_matexp_vecs_grads.cc matadd_coeff_v3.cu.o matmul_v2.cu.o matadd_coeff_v2.cu.o -I $TF_INC -I /usr/local/cuda/include -fPIC -lcudart
+
