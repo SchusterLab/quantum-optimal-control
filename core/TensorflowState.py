Added no scaling and squaring option and fixed bugs

core/TensorflowState.py
@@ -173,7 +173,7 @@ class TensorflowState:
             self.raw_weight = []
 
             #initial_xy_stddev = (0.1/np.sqrt(self.sys_para.control_steps))
-            initial_stddev = (0.1/np.sqrt(self.sys_para.steps))
+            initial_stddev = (10./np.sqrt(self.sys_para.steps))
             if self.sys_para.Dts != []:
                 self.raw_weight = []
                 if self.sys_para.ops_len - len(self.sys_para.Dts) > 0:
@@ -187,7 +187,7 @@ class TensorflowState:
                         current = tf.concat(0,[current,weights[ii+1,:]])
                     self.current = tf.reshape(current,[1, (self.sys_para.ops_len - len(self.sys_para.Dts))*self.sys_para.steps])
                 else:
-                    initial_stddev = (0.1/np.sqrt(self.sys_para.ctrl_steps[0]))
+                    initial_stddev = (10/np.sqrt(self.sys_para.ctrl_steps[0]))
                     weights = tf.truncated_normal([1 ,self.sys_para.ctrl_steps[0]],
                                                                    mean= initial_guess ,dtype=tf.float32,
                             stddev=initial_stddev )
@@ -196,7 +196,7 @@ class TensorflowState:
                     self.current = weights
 
                 for ii in range (len(self.sys_para.Dts)-index):
-                    initial_stddev = (0.1/np.sqrt(self.sys_para.ctrl_steps[ii+index]))
+                    initial_stddev = (10/np.sqrt(self.sys_para.ctrl_steps[ii+index]))
                     weight = tf.truncated_normal([1 ,self.sys_para.ctrl_steps[ii+index]],
                                                                    mean= initial_guess ,dtype=tf.float32,
                             stddev=initial_stddev )

system/SystemParametersGeneral.py
@@ -7,9 +7,10 @@ from scipy.special import factorial
 
 class SystemParametersGeneral:
 
-    def __init__(self,H0,Hops,Hnames,U,U0,total_time,steps,states_forbidden_list,states_concerned_list,multi_mode,maxA, draw,initial_guess,evolve, evolve_error, show_plots, H_time_scales,Unitary_error,state_transfer):
+    def __init__(self,H0,Hops,Hnames,U,U0,total_time,steps,states_forbidden_list,states_concerned_list,multi_mode,maxA, draw,initial_guess,evolve, evolve_error, show_plots, H_time_scales,Unitary_error,state_transfer,no_scaling):
         # Input variable
         self.state_transfer = state_transfer
+        self.no_scaling = no_scaling
        
         self.H0_c = H0
         self.ops_c = Hops
@@ -106,7 +107,7 @@ class SystemParametersGeneral:
         else:
             self.div = d
         
-        if self.state_transfer:
+        if self.state_transfer or self.no_scaling:
             self.div =0
         while True:
             for ii in range (self.steps):
@@ -206,7 +207,7 @@ class SystemParametersGeneral:
         
         self.exps =[]
         self.divs = []
-        if self.state_transfer:
+        if self.state_transfer or self.no_scaling:
             comparisons = 1
         else:
             comparisons = 5