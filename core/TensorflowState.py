a18017b74a8d2fbf9fb9247f30b18a56bb723e6f
Nelson Leung <nleung91@gmail.com>
2016-10-21 15:33:25 -0500

    edit grad defun

--- core/TensorflowState.py
+++ core/TensorflowState.py
@@ -63,19 +63,40 @@ class TensorflowState:
 
             return matexp 
         
-        @ops.RegisterGradient("matvecexp_op")
-        def matvecexp_op_grad(op, grad):  
+        @function.Defun(tf.float32,tf.float32,tf.float32,tf.float32)
+        def matvecexp_op_grad(uks,H_all,psi, grad):  
 
             coeff_grad = []
 
             coeff_grad.append(tf.constant(0,dtype=tf.float32))
+            
+            ### get output of the function
+            I = H_all[input_num]
+            matvecexp = psi
+        #     H = I-I
+
+            uks_Hk_list = []
+
+            for ii in range(input_num):
+                uks_Hk_list.append(uks[ii]*H_all[ii])
+        #         H = H + uks[ii]*H_all[ii]
+            H = tf.add_n(uks_Hk_list)
+
+            psi_n = psi
+            factorial = 1.
+
+            for ii in range(1,taylor_terms):      
+                factorial = factorial * ii
+                psi_n = tf.matmul(H,psi_n)
+                matvecexp = matvecexp + psi_n/factorial
+            #####
+            
+            
             for ii in range(1,input_num):
                 coeff_grad.append(tf.reduce_sum(tf.mul(grad,
-                       tf.matmul(op.inputs[1][ii],op.outputs[0]))))
+                       tf.matmul(H_all[ii],matvecexp))))
                 
              
-            uks = op.inputs[0]
-            H_all = op.inputs[1]
             
             I = H_all[input_num]
             vec_grad = grad
@@ -94,10 +115,10 @@ class TensorflowState:
                 vec_grad_n = tf.matmul(H,vec_grad_n)
                 vec_grad = vec_grad + vec_grad_n/factorial
 
-            return [tf.pack(coeff_grad), tf.zeros(tf.shape(op.inputs[1]),dtype=tf.float32),vec_grad]                                         
+            return [tf.pack(coeff_grad), tf.zeros(tf.shape(H_all),dtype=tf.float32),vec_grad]                                         
         global matvecexp_op
         
-        @function.Defun(tf.float32,tf.float32,tf.float32, python_grad_func=matvecexp_op_grad)                       
+        @function.Defun(tf.float32,tf.float32,tf.float32, grad_func=matvecexp_op_grad)                       
         def matvecexp_op(uks,H_all,psi):
             
             I = H_all[input_num]
