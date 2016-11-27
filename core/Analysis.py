7f45fd163d315c2b3371406791b6260703215b70
Nelson Leung <nleung91@gmail.com>
2016-11-26 18:31:05 -0600

    used tf.scan for unitary propagation

--- core/Analysis.py
+++ core/Analysis.py
@@ -59,19 +59,24 @@ class Analysis:
         
         inter_vecs = tf.pack(self.tf_inter_vecs).eval()
         
+        if self.sys_para.state_transfer:
+            inter_vecs = np.transpose(inter_vecs, axes=(0, 2, 1))
+        
         if self.sys_para.save:
             with H5File(self.sys_para.file_path) as hf:
-                hf.append('inter_vecs_raw_real',np.array(inter_vecs[:,:,0:state_num]))
-                hf.append('inter_vecs_raw_imag',np.array(inter_vecs[:,:,state_num:2*state_num]))
+                hf.append('inter_vecs_raw_real',np.array(inter_vecs[:,0:state_num,:]))
+                hf.append('inter_vecs_raw_imag',np.array(inter_vecs[:,state_num:2*state_num,:]))
         
         for inter_vec in inter_vecs:
-            inter_vec_real = (inter_vec[:,0:state_num])
-            inter_vec_imag = (inter_vec[:,state_num:2*state_num])
+            
+            
+            inter_vec_real = (inter_vec[0:state_num,:])
+            inter_vec_imag = (inter_vec[state_num:2*state_num,:])
             inter_vec_c = inter_vec_real+1j*inter_vec_imag
 
             if self.sys_para.is_dressed:
 
-                dressed_vec_c= np.transpose(np.dot(np.transpose(v_sorted),np.transpose(inter_vec_c)))
+                dressed_vec_c= np.dot(np.transpose(v_sorted),inter_vec_c)
                 
                 inter_vec_mag_squared = np.square(np.abs(dressed_vec_c))
                 
